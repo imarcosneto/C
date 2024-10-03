9229121ff05675597057b13fbdc4a3184c752b0e
Marcos Neto <141196730+imarcosneto@users.noreply.github.com>
2024-10-03 20:12:11 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -1,7 +1,7 @@
 #include <stdio.h> 
 int main(){
     int a;
-    printf("\nEntre com um número do array correspondente a fila primária: ");
+    printf("\nEntre com um inteiro correspondente a um valor da fila primária: ");
     scanf("%d", &a);
     
     if(a>=1 && a<=24){
